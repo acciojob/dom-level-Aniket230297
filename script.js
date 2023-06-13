ba204c8eba7113bb322e6cb7840475939963945a
Aniket Kule <38484786+Aniket230297@users.noreply.github.com>
2023-06-13 18:05:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+window.addEventListener('DOMContentLoaded', (event) => {
+      let element = document.querySelector('#level');
+      let level = 0;
+
+      while (element.parentNode !== null) {
+        element = element.parentNode;
+        level++;
+      }
+
+      const result = document.createElement('p');
+      result.textContent = `The level of the element is: ${level}`;
+      document.body.appendChild(result);
+    });
